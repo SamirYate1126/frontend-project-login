ca9d59f3a87cbc68ca84a08be7e56408d66c1e21
SamirYate1126 <131462250+SamirYate1126@users.noreply.github.com>
2025-03-01 19:22:25 -0500

    Update Routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import PageAdmin from "./pages/PageAdmin";
 import ProtectedRoute from "./components/ProtectedRoute";
 import PageTeacher from "./pages/PageTeacher";
 import PageStudent from "./pages/PageStudent";
+import Recovery from "./pages/Recovery";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Login />} />
         <Route element={<ProtectedRoute/>}>
+          <Route path="/register" element={<Register />} />
           <Route path="/admin" element={<PageAdmin />} />
           <Route path="/student" element={<PageStudent />} />
           <Route path="/teacher" element={<PageTeacher/>} />
         </Route>
+        <Route path="/recovery-password" element={<Recovery/>}/>
       </Routes>
     </BrowserRouter>
   );
